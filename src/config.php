5ca386b80b50bebfe6863223449ebb9e811e32e9
Mr.tao <staugur@saintic.com>
2019-06-13 17:19:42 +0800

    chore: set execute

--- src/config.php
+++ src/config.php
@@ -4,10 +4,10 @@
 define('VERSION', '0.1.0');
 
 // REDIS connection information in the DSN-style format: redis://[user:password]@host[:port/db]
-define('REDIS', 'redis://:abc@127.0.0.1:6379/0');
+define('REDIS', 'redis://:XingkaOps@127.0.0.1:6379/1');
 
 // Verification token filled in at the time of registration at the central end
-define('TOKEN', '');
+define('TOKEN', 'test');
 
 // Set this service status: ready or tardy
 define('STATUS', 'ready');
